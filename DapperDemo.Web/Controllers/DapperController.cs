d076f5bbcc9cbf8ea05d2c8f2146548dc50127b8
Fillin <collegetang@outlook.com>
2020-07-03 08:48:48 +0800

    Oracle Query

--- DapperDemo.Web/Controllers/DapperController.cs
+++ DapperDemo.Web/Controllers/DapperController.cs
@@ -21,8 +21,20 @@ namespace DapperDemo.Web.Controllers
         [HttpGet]
         public List<dynamic> GetAll()
         {
-            dynamic list = _client.Query<dynamic>(@"select * from testUser");
+            dynamic list = _client.Query<dynamic>(@"select * from testUser where username='fillin'");
             return list;
+        }
+        /// <summary>
+        /// 根据用户名查询用户信息
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns></returns>
+        [HttpPost] 
+        public dynamic GetByUserName(string userName)
+        {
+            string sql = @"select * from testuser where username=@Username";
+            dynamic user = _client.Query<dynamic>(sql, new { Username = userName });
+            return user;
         }
     }
 }
